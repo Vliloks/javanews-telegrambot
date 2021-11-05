6340f9e37d5770ce6f6a577698c7139925b57a5a
vliloks <vlilialoks@gmail.com>
2021-11-05 23:33:48 +0300

    JNTB-0: added SpringBoot skeleton to the project.

---
+++ src/main/java/com/github/vliloks/jntb/JavanewsTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.vliloks.jntb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavanewsTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavanewsTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/vliloks/jntb/JavanewsTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vliloks.jntb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavanewsTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
