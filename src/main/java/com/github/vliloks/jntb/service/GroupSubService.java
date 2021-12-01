5596e58ee30d9d9c30fed0e7c446bce4e7fc7cba
vliloks <vlilialoks@gmail.com>
2021-12-01 18:55:37 +0300

    STEP_7 JNTB-7: added the ability to delete group subscription.

--- src/main/java/com/github/vliloks/jntb/service/GroupSubService.java
+++ src/main/java/com/github/vliloks/jntb/service/GroupSubService.java
@@ -3,7 +3,11 @@ package com.github.vliloks.jntb.service;
 import com.github.vliloks.jntb.javarushclient.dto.GroupDiscussionInfo;
 import com.github.vliloks.jntb.repository.entity.GroupSub;
 
+import java.util.Optional;
+
 public interface GroupSubService {
 
     GroupSub save(Long chatId, GroupDiscussionInfo groupDiscussionInfo);
+    GroupSub save(GroupSub groupSub);
+    Optional<GroupSub> findById(Integer id);
 }
